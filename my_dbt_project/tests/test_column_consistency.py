f552ded20597f8fb6722f03890ed8fd68ad51a1b
aunsine <aunsine@gmail.com>
2025-07-10 17:35:58 +0200

    update column check

--- my_dbt_project/tests/test_column_consistency.py
+++ my_dbt_project/tests/test_column_consistency.py
@@ -5,12 +5,29 @@ import json
 import pandas as pd
 import pytest
 
-DATA_DIR = "dbt/input"
+DATA_DIR = "input/"
 FILES = ["properties_1.txt", "properties_2.txt", "properties_3.txt"]
 
-EXPECTED_COLUMNS = [
-    "property_id", "price_gbp", "listed_date",
-    "latitude", "longitude", "property_type"
+EXPECTED_COLUMNS = [   
+"price",
+"type",
+"address",
+"url",
+"agent_url",
+"number_bedrooms",
+"full_description",
+"agent_name",
+"agent_address",
+"postcode",
+"longitude",
+"latitude",
+"viewtype",
+"propertytype",
+"propertysubtype",
+"added",
+"maxsizeft",
+"retirement",
+"preowned",
 ]
 
 def load_json_split_columns(file_path):
@@ -20,6 +37,8 @@ def load_json_split_columns(file_path):
 
 @pytest.mark.parametrize("filename", FILES)
 def test_columns_match_expected(filename):
-    actual = load_json_split_columns(os.path.join(DATA_DIR, filename))
+
+    print(f"Actual testing file: {os.path.join(DATA_DIR, filename)}")
+    actual = load_json_split_columns(os.path.join(DATA_DIR, filename))    
     cleaned = [c.strip().lower().replace(" ", "_") for c in actual]
     assert cleaned == EXPECTED_COLUMNS, f"Mismatch in columns: {filename}"
