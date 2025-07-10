6668ae33bb8987c3e3d9a2cfe8e99142029c8c8d
aunsine <aunsine@gmail.com>
2025-07-10 16:05:04 +0200

    Add new calculated field

---
+++ models/serving/srv_properties.sql
@@ -0,0 +1,22 @@
+{{ 
+    config(
+        materialized='incremental',
+        tags=['staging', 'properties'],
+        incremental_strategy='merge',
+        unique_key= ['url'],
+        partition_by={
+            "field": "added",
+            "data_type": "date",
+            "granularity": "day"
+        },
+        post_hook= ['DELETE FROM {{this}} WHERE url IS NULL OR price IS NULL' ]
+
+    )
+}}
+
+SELECT *, 
+SAFE_DIVIDE(price, maxSizeFt) AS cost_per_feet,
+FROM {{ref('stg_properties')}}
+{% if is_incremental() %}
+WHERE added > (SELECT MAX(added) FROM {{ this }})
+{% endif %}
